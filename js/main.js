453fb2aea4dcb6fb3c4b8a6e8421299a5d62e68f
Victoria <108477893+victoriaValt@users.noreply.github.com>
2024-02-23 15:59:17 +0300

    browser window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 function yesPage() {
      window.location.href = "yes.html";
 }
